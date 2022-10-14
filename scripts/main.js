7009f4331975505fc2e5d9fab2abafe6547f32b8
KBDDEY <112295933+KBDDEY@users.noreply.github.com>
2022-10-14 11:55:33 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,6 @@ myImage.onclick = function() {
 // 获取新按钮和标题的引用
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-
 // 个性化欢迎信息设置函数
 function setUserName() {
   let myName = prompt('请输入你的名字。');
@@ -25,7 +24,6 @@ function setUserName() {
     myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
   }
 }
-
 // 初始化代码：在页面初次读取时进行构造工作：
 if (!localStorage.getItem('name')) {
   setUserName();
@@ -33,7 +31,6 @@ if (!localStorage.getItem('name')) {
   let storedName = localStorage.getItem('name');
   myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
 }
-
 // 为按钮设置 onclick 事件处理器：
 myButton.onclick = function() {
   setUserName();
