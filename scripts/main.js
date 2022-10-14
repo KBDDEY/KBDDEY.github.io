527ad815c5beb33f2baf41442d02ac48bcde334b
KBDDEY <112295933+KBDDEY@users.noreply.github.com>
2022-10-14 09:29:10 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if (mySrc === 'D:/web-projects/img/hutao.png') {
-    myImage.setAttribute ('src','D:/web-projects/img/hutao2.png');
+  if (mySrc === 'img/hutao.png') {
+    myImage.setAttribute ('src','img/hutao2.png');
   } else {
-    myImage.setAttribute ('src','D:/web-projects/img/hutao.png');
+    myImage.setAttribute ('src','/img/hutao.png');
   }
 };
 
