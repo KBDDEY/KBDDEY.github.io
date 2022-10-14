da88772ad135a37982d0f2f5427f4ae3fea9ff73
KBDDEY <112295933+KBDDEY@users.noreply.github.com>
2022-10-14 11:53:54 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
 // 设置图片切换
-let myImage = document.querySelector(img);
+let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-  let mySrc = myImage.getAttribute(src);
+  let mySrc = myImage.getAttribute('src');
   if (mySrc === img/hutao.png) {
-    myImage.setAttribute (src,img/hutao2.png);
+    myImage.setAttribute ('src',img/hutao2.png);
   } else {
-    myImage.setAttribute (src,img/hutao.png);
+    myImage.setAttribute ('src',img/hutao.png);
   }
 };
 
 // 设置个性化欢迎信息
 // 获取新按钮和标题的引用
-let myButton = document.querySelector(button);
-let myHeading = document.querySelector(h1);
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
 // 个性化欢迎信息设置函数
 function setUserName() {
@@ -21,16 +21,16 @@ function setUserName() {
   if (!myName || myName === null) {
     setUserName();
   } else {
-    localStorage.setItem(name, myName);
+    localStorage.setItem('name', myName);
     myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
   }
 }
 
 // 初始化代码：在页面初次读取时进行构造工作：
-if (!localStorage.getItem(name)) {
+if (!localStorage.getItem('name')) {
   setUserName();
 } else {
-  let storedName = localStorage.getItem(name);
+  let storedName = localStorage.getItem('name');
   myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
 }
 
